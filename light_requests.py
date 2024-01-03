b1b6e98ca67a2262637803ff396f145e3ce266d6
SamTipper <rogoclock@gmail.com>
2024-01-03 14:08:18 +0000

    initial commit

---
+++ light_requests.py
@@ -0,0 +1,76 @@
+import requests
+from json import dumps
+
+colors_dict = {
+    "off": (0, 0, 0),
+    "white": (255, 255, 255),
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "cyan": (0, 255, 255),
+    "magenta": (255, 0, 255),
+    "orange": (255, 165, 0),
+    "purple": (128, 0, 128),
+    "lime": (0, 255, 0),
+    "pink": (255, 182, 193),
+    "turquoise": (64, 224, 208),
+    "brown": (165, 42, 42),
+    "teal": (0, 128, 128),
+    "lavender": (230, 230, 250)
+}
+
+def colour_change_body(device_mac: str, model_num: str, colour: tuple = None, rgb: tuple = None) -> dict | None:
+    if colour:
+        if not colour in colors_dict:
+            return
+
+        R, G, B = colors_dict[colour]
+
+    elif rgb:
+        R, G, B = rgb
+
+    else:
+        R, G, B = 255, 255, 255
+
+    request_body = {
+        "device": device_mac,
+        "model": model_num,
+        "cmd": {
+            "name": "color",
+            "value": {
+                "r": R,
+                "g": G,
+                "b": B
+            }
+        }
+	}
+
+    return request_body
+
+
+def brightness_change_body(device_mac: str, model_num: str, brightness: int) -> dict:
+    request_body = {
+        "device": device_mac,
+        "model": model_num,
+        "cmd": {
+			"name": "brightness",
+			"value": brightness
+        }
+	}
+
+    return request_body
+    
+
+def PUT(api_key: str, request_body: str) -> bool:
+    params = {
+        "Govee-API-Key": api_key,
+        "Content-Type":  "application/json"
+    }
+
+    request = requests.put("https://developer-api.govee.com/v1/devices/control", headers=params, data=dumps(request_body))
+    return True if request.status_code == 200 else False
+    
+
+def all_colours() -> list:
+    return colors_dict.keys()
---
+++ main.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext.commands import cooldown, BucketType, CommandOnCooldown
+from dotenv import load_dotenv
+from os import environ
+
+import light_requests
+
+load_dotenv()
+
+API_KEY   = environ['API_KEY']
+MAC_ADDR  = environ['DEVICE_MAC']
+MODEL_NUM = environ['DEVICE_MODEL_NUM']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+intents = discord.Intents.default()
+client = discord.Bot(intents=intents)
+
+
+@client.event
+async def on_application_command_error(ctx, error):
+    if isinstance(error, CommandOnCooldown):
+        await ctx.respond(error)
+
+
+@client.slash_command()
+@cooldown(1, 60, BucketType.guild)
+async def setcolour(ctx, colour):
+    request_body = light_requests.colour_change_body(MAC_ADDR, MODEL_NUM, colour=colour.lower())
+
+    if not request_body:
+        await ctx.respond(f"{colour.title()} is not a valid colour.")
+        await ctx.send(f"All colours:\n{', '.join(light_requests.all_colours()).title()}")
+        return
+
+    success = light_requests.PUT(API_KEY, request_body)
+
+    if success:
+        await ctx.respond(f"Changing the light colour to {colour.title()}.")
+    else:
+        await ctx.respond(f"There was an error while changing the light colour to {colour.title()}.")
+
+
+@client.slash_command()
+@cooldown(1, 60, BucketType.guild)
+async def setrgb(ctx, r, g, b):
+    if not r.strip().isdigit() or not g.strip().isdigit() or not b.strip().isdigit():
+        await ctx.respond("Please enter numerical values.")
+        return
+
+    r, g, b = int(r), int(g), int(b)
+
+    if not (0 <= r <= 255) or not (0 <= g <= 255) or not (0 <= b <= 255):
+        await ctx.respond("Min: 0, Max: 255 for RGB values.")
+        return
+
+    request_body = light_requests.colour_change_body(MAC_ADDR, MODEL_NUM, rgb=(r, g, b))
+    success = light_requests.PUT(API_KEY, request_body)
+
+    if success:
+        await ctx.respond(f"Changing the light colour to R: {r}, G: {g}, B: {b}.")
+    else:
+        await ctx.respond(f"There was an error while changing the light colour to R: {r}, G: {g}, B: {b}.")
+
+
+@client.slash_command()
+@cooldown(1, 60, BucketType.guild)
+async def setbrightness(ctx, brightness):
+    if not brightness.strip().replace("%", "").isdigit():
+        await ctx.respond("The brightness value must be a number.")
+        return
+
+    brightness = int(brightness.strip().replace("%", ""))
+
+    if not brightness in range(1, 101):
+        await ctx.respond("The brightness value must be between 1-100.")
+        return
+
+    request_body = light_requests.brightness_change_body(MAC_ADDR, MODEL_NUM, brightness)
+    success = light_requests.PUT(API_KEY, request_body)
+
+    if success:
+        await ctx.respond(f"Changing the light brightness to {brightness}%")
+    else:
+        await ctx.respond(f"There was an error while changing the light brightness.")
+
+
+@client.slash_command()
+async def allcolours(ctx):
+    await ctx.respond(f"All colours:\n{', '.join(light_requests.all_colours()).title()}")
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+client.run(BOT_TOKEN)
